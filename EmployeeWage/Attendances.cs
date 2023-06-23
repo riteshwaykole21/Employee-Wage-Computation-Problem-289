66e5a3040d7e2c476bd22046e5628c3af2df01af
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-23 19:05:30 +0530

    [Ritesh]add :Calculate Daily Employee Wage

--- EmployeeWage/Attendances.cs
+++ EmployeeWage/Attendances.cs
@@ -11,16 +11,23 @@ namespace EmployeeWage
         public void Check()
         {
             int present = 1;
+            int wageperhrs = 20;
+            int empHrs = 0;
+            int empwage = 0;
             Random random = new Random();
             int check = random.Next(2);
-            if(check == present)
+            if (check == present)
             {
                 Console.WriteLine("Employee Is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee Is Absent");
+                empHrs = 0;
             }
+            empwage = wageperhrs * empHrs;
+            Console.WriteLine(empwage);
         }
     }
 }
