c48ed962af663e8d60cb3a552644eb245ed6f95c
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-23 18:42:04 +0530

    [Ritesh]add : Check Attendances Of The Employee

---
+++ EmployeeWage/Attendances.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Attendances
+    {
+        public void Check()
+        {
+            int present = 1;
+            Random random = new Random();
+            int check = random.Next(2);
+            if(check == present)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation");
+            Attendances attendances = new Attendances();
+            attendances.Check();
+            Console.ReadLine();
+        }
+    }
+}
