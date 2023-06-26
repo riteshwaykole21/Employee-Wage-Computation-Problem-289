2968df51442e006c8053cac004e5e693e1894d53
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-26 08:32:45 +0530

    [Ritesh]add : Wage For Months

--- EmployeeWage/Attendances.cs
+++ EmployeeWage/Attendances.cs
@@ -13,26 +13,30 @@ namespace EmployeeWage
             const int isfulltime = 1;
             const int isparttime = 2; 
             const int wageperhrs = 20;
+            const int workingday = 20;
             int empHrs = 0;
             int empwage = 0;
-            Random random = new Random();
-            int check = random.Next(0,3);
-            switch(check)
+            for(int i = 1;i <= workingday;i++)
             {
-                case isfulltime:
-                    Console.WriteLine("Employee Present Full Time");
-                    empHrs = 8;
-                    break;
-                case isparttime:
-                    Console.WriteLine("Employee Present Part Time");
-                    empHrs = 4;
-                    break;
-                 default:
-                    Console.WriteLine("Employee Is Absent");
-                    break;
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case isfulltime:
+                        Console.WriteLine("Employee Present Full Time");
+                        empHrs = 8;
+                        break;
+                    case isparttime:
+                        Console.WriteLine("Employee Present Part Time");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee Is Absent");
+                        break;
+                }
+                empwage += wageperhrs * empHrs;
             }
-            empwage = wageperhrs * empHrs;
-            Console.WriteLine(empwage);
+            Console.WriteLine("Total Employee Wage " + empwage);
         }
     }
 }
